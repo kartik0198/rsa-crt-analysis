7589fcfe3c59d01322269bfef710944f8abd99c2
Kartik <kartik@trell.in>
2020-10-06 23:57:45 +0530

    readme change and code cleanup

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # CryptoProjectRSA
-RSA and CRT implementation
+The project was made as part of a research and analysis study conducted to understand the difference between RSA & RSA-CRT and finally infer which is the better one.
 
 Implemented the standard RSA algorithm,
 understanding its deficiency and to further optimize it through combining the RSA algorithm with Chinese
 Remainder Theorem to speed up RSA decryption by an
 approximate factor of four. Both the implementations are done in Java.
 
-We then compare and analyse the performance of the variant for which graphs have been attatched.
+We then compare and analyse the performance of the variant for which graphs have been attached.
 
 The RSA-CRT variant is backwards compatible in the sense that a
-system using it can interoperate with systems using standard RSA.
+system using it can inter-operate with systems using standard RSA.
 
-In future, a GUI to convert text is required.
+In future, a GUI based tool to encrypt/decrypt plain text will be made.
---
+++ src/com/kartik/CRT.java
@@ -0,0 +1,146 @@
+package com.kartik;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CRT {
+
+    private static BigInteger P;
+    private static BigInteger Q;                                // large prime numbers p and q.
+    private static BigInteger N;                                // N
+    private static BigInteger phi;                              // Phi(N) = ( p - 1 ) * ( q - 1 )
+    private static BigInteger E = BigInteger.valueOf(3);        // Public E initially taken as 65537 since its recommended
+    private static BigInteger D;                                // Private D
+    private static BigInteger dP, dQ;
+    private static BigInteger qInverse;
+    private static final int bitLength = 1024;
+
+    private static void createKeys() {
+        Random rand1 = new Random(System.currentTimeMillis());
+        Random rand2 = new Random(System.currentTimeMillis() * 10);
+
+        /* probablePrime is used to randomly generate positive number that is probably prime */
+        P = BigInteger.probablePrime(bitLength / 2, rand1);
+        Q = BigInteger.probablePrime(bitLength / 2, rand2);
+        phi = P.subtract(BigInteger.valueOf(1)).multiply(Q.subtract(BigInteger.valueOf(1)));
+        N = P.multiply(Q);
+        while (true) {
+            BigInteger GCD = phi.gcd(E);
+            if ((GCD.equals(BigInteger.ONE)) && (E.compareTo(phi) < 0) && (E.compareTo(BigInteger.ONE) > 0)) {
+                break;
+            } else {
+                E = E.add(BigInteger.ONE);
+            }
+        }
+        D = E.modInverse(phi);
+        dP = (D.mod(P.subtract(BigInteger.ONE)));
+        dQ = (D.mod(Q.subtract(BigInteger.ONE)));
+        qInverse = Q.modInverse(P);
+        System.out.println("** Keys generated **");
+        System.out.println("The Public Key Pair is: {" + E + "," + N + "]");
+        System.out.println("The Private Key Pair is: {" + D + "," + N + "]");
+    }
+
+    private static void useKeys() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter P and Q");
+        P = sc.nextBigInteger();
+        Q = sc.nextBigInteger();
+        //E = sc.nextBigInteger();
+        phi = P.subtract(BigInteger.valueOf(1)).multiply(Q.subtract(BigInteger.valueOf(1)));
+        N = P.multiply(Q);
+        D = E.modInverse(phi);
+        dP = (D.mod(P.subtract(BigInteger.ONE)));
+        dQ = (D.mod(Q.subtract(BigInteger.ONE)));
+        System.out.println("** Keys generated **");
+        System.out.println("The Public Key Pair is: {" + E + "," + N + "]");
+        System.out.println("The Private Key Pair is: {" + D + "," + N + "]");
+    }
+
+    private static void encryptDecryptUseKeys(String message) {
+        byte[] cipher;
+        byte[] plain;
+        String cipherTxt;
+        String plainTxt;
+        StringBuilder ans = new StringBuilder();
+        StringBuilder ans2 = new StringBuilder();
+        System.out.println();
+        for (int i = 0; i < message.length(); i++) {
+            byte[] msg = message.substring(i, i + 1).getBytes();
+            cipher = encrypt(msg);
+            cipherTxt = new String(cipher);
+            ans.append(cipherTxt);
+            plain = decrypt(cipher);
+            plainTxt = new String(plain);
+            ans2.append(plainTxt);
+        }
+        System.out.println("CIPHER TEXT=" + ans);
+        System.out.println("DECRYPTED TEXT =" + ans2);
+    }
+
+    private static byte[] encrypt(byte[] message) {
+        BigInteger msg = new BigInteger(message);
+        return msg.modPow(E, N).toByteArray();
+    }
+
+    private static byte[] decrypt(byte[] message) {
+        BigInteger msg = new BigInteger(message);
+        BigInteger m1 = msg.modPow(dP, P);
+        BigInteger m2 = msg.modPow(dQ, Q);
+        BigInteger temp = qInverse.multiply(m1.subtract(m2));
+        BigInteger h = temp.mod(P);
+        BigInteger m = m2.add(h.multiply(Q));
+        return m.toByteArray();
+    }
+
+    public static void main(String[] args) {
+        long startTimeKeyGen = System.currentTimeMillis();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("****** CRYPTOGRAPHY PROJECT ******");
+        System.out.println("Press 1 to generate keys for RSA-CRT or Press 2 to enter your own values.");
+        int input = sc.nextInt();
+        if (input == 1) {
+            createKeys();
+        } else {
+            useKeys();
+        }
+        long endTimeKeyGen = System.currentTimeMillis();
+        long totalTimeKeyGen = endTimeKeyGen - startTimeKeyGen;
+        System.out.println("The time taken to generate Keys: " + totalTimeKeyGen);
+        int token = 1;
+        while (token == 1) {
+            System.out.println("Enter the message to be encrypted : ");
+            Scanner sc1 = new Scanner(System.in);
+            String message = sc1.nextLine();
+            byte[] msg = message.getBytes();
+            if (input == 1) {
+                System.out.println("*** ENCRYPTION STARTS ***");
+                long startTime = System.currentTimeMillis();
+                byte[] cipher = encrypt(msg);
+                String cipherTxt = new String(cipher);
+                System.out.println("*** ENCRYPTION DONE ***");
+                long endTime = System.currentTimeMillis();
+                long totalTime = endTime - startTime;
+                System.out.println("The time taken in RSA-CRT encryption: " + totalTime);
+                System.out.println("Cipher text: ");
+                System.out.println(cipherTxt);
+                System.out.println("*** DECRYPTION STARTS ***");
+                long startTime1 = System.currentTimeMillis();
+                byte[] plain = decrypt(cipher);
+                String plainTxt = new String(plain);
+                System.out.println("*** DECRYPTION DONE ***");
+                long endTime1 = System.currentTimeMillis();
+                long totalTime1 = endTime1 - startTime1;
+                System.out.println("The time taken in RSA-CRT decryption: " + totalTime1);
+                System.out.println("Plain text: ");
+                System.out.println(plainTxt);
+            } else {
+                encryptDecryptUseKeys(message);
+            }
+            System.out.println("Do you to enter again? (1/0)");
+            Scanner sc2 = new Scanner(System.in);
+            if (sc2.nextInt() == 0) token = 0;
+        }
+    }
+}
